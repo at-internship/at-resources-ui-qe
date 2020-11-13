375f9defc2faee0d1a20e39c083cfeaabc62caca
AdanMerino-AT <brayan.merino@agilethought.com>
2020-11-12 18:48:41 -0600

    Build CreateStories feature and Hooks

---
+++ src/test/java/com/stepdefinitions/CreateStories.java
@@ -0,0 +1,103 @@
+package com.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class CreateStories {
+	@Given("I am in the Stories Administration page")
+	public void i_am_in_the_Stories_Administration_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I do click in the AddNewStory button")
+	public void i_do_click_in_the_AddNewStory_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The AddNewStory form should be displayed")
+	public void the_AddNewStory_form_should_be_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I select a sprint")
+	public void i_select_a_sprint() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I select a user")
+	public void i_select_a_user() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I select a priority")
+	public void i_select_a_priority() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I put a name")
+	public void i_put_a_name() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I put a description")
+	public void i_put_a_description() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I put acceptance criteria")
+	public void i_put_acceptance_criteria() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I assign story points")
+	public void i_assign_story_points() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I select a start date")
+	public void i_select_a_start_date() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I select a due date")
+	public void i_select_a_due_date() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I do click in the Save button")
+	public void i_do_click_in_the_Save_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I should see an alert of successful story creation")
+	public void i_should_see_an_alert_of_successful_story_creation() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The story should be found by the search function of the page")
+	public void the_story_should_be_found_by_the_search_function_of_the_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The story should be found in the DB")
+	public void the_story_should_be_found_in_the_DB() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
