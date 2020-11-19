715efaf96be8e6fb9fcb912277d12ce836b0b58d
= <pakoo_15@hotmail.com>
2020-11-18 22:21:57 -0600

    Add functions to the delete stories process

--- src/test/java/com/globalClasses/TestBase.java
+++ src/test/java/com/globalClasses/TestBase.java
@@ -7,6 +7,7 @@ public class TestBase {
 	protected WebDriver driver = Hooks.getDriver();
 	protected BasePages base = new BasePages(driver);
 	protected GetAllStories usersAP = new GetAllStories();
+	protected DeleteByStory deleteAP = new DeleteByStory();
 	protected TestValues values = new TestValues();
 	protected StoriesAdmonPage storiesAP = new StoriesAdmonPage();
 	protected AddNewStoryPage addNSP = new AddNewStoryPage();
